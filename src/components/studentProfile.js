2e40741b1face499e2590fbabece050a0d639b2e
JayRome1505 <13903051@student.uts.edu.au>
2022-08-23 20:12:53 +1000

    Created form for first login. (ugly asf) - Dynamic name - Text field for preferred name - Selection box for year and degree - Submit button

--- src/components/studentProfile.js
+++ src/components/studentProfile.js
@@ -10,6 +10,9 @@ class StudentProfile extends React.Component {
 
     this.state = {
       studentName: "",
+      preferredName: '',
+      year: 'yr1',
+      degree: 'deg1',
     };
   }
 
@@ -30,6 +33,24 @@ class StudentProfile extends React.Component {
     this.componentDidMount();
   };
 
+  handlePreferredNameChange = (event) => {
+    this.setState({
+      username: event.target.value
+    })
+  }
+
+  handleYearChange = (event) => {
+    this.setState({
+      year: event.target.value
+    })
+  }
+
+  handleDegreeChange = (event) => {
+    this.setState({
+      year: event.target.value
+    })
+  }
+
   render() {
 
     const { studentName } = this.state;
@@ -37,7 +58,35 @@ class StudentProfile extends React.Component {
     return (
       <Form style={{textAlign: "center", maxWidth: '100%', fontFamily: "Times New Roman"}} className="form">
         <h3 style={{color: "light grey"}}>My Profile <i>{studentName}</i></h3>
-    
+
+        <form>
+          <div>
+            <label>Name:</label>
+            <p> {this.state.studentName}</p>
+          </div>
+          <div>
+            <label>Preferred Name:</label>
+            <input type='text' value={this.state.preferredName} onChange={this.handlePreferredNameChange}/>
+          </div>
+          <div>
+            <label>Year:</label>
+            <select value={this.state.year} onChange={this.handleYearChange}>
+              <option value="yr1">Yr 1</option>
+              <option value="yr2">Yr 2</option>
+              <option value="yr3">Yr 3</option>
+            </select>
+          </div>
+          <div>
+            <label>Degree:</label>
+            <select value={this.state.year} onChange={this.handleDegreeChange}>
+              <option value="deg1">Software Engo</option>
+              <option value="deg2">Finance</option>
+              <option value="deg3">Philosophy</option>
+            </select>
+          </div>
+
+          <button type="submit"> Submit </button>
+        </form> 
       </Form>
     );
   };
