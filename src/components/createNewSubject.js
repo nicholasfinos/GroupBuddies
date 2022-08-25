02e45be8c2b1962a08b784248658845925906241
your-username <grace.billiris@gmail.com>
2022-08-25 22:49:02 +1000

    Updating UI constraints

--- src/components/createNewSubject.js
+++ src/components/createNewSubject.js
@@ -2,6 +2,18 @@ import React from "react";
 import { Button } from "@material-ui/core";
 import SubjectDataService from "../services/subject-service";
 
+
+const required = (value) => {
+  if (!value) {
+    return (
+      <div className="alert alert-danger" role="alert">
+        This field is required!
+      </div>
+    );
+  }
+};
+
+
 class CreateSubject extends React.Component {
   constructor(props) {
     super(props);
@@ -18,7 +30,8 @@ class CreateSubject extends React.Component {
       groupAssessment: "",
       semester: "",
       subjectTopics: "",
-      username: ""
+      username: "",
+      submitted: false
     };
   }
 
@@ -29,7 +42,6 @@ class CreateSubject extends React.Component {
   }
 
   onChangeGroupAssessment(e) {
-    //Get List Size, compare with number tutorial 
     this.setState({groupAssessment: e.target.value});
   }
   
@@ -68,7 +80,8 @@ class CreateSubject extends React.Component {
           tutorialNumbers: response.data?.tutorialNumbers,
           groupAssessment: response.data?.groupAssessment,
           semester: response.data?.semester,
-          subjectTopics: response.data?.subjectTopics
+          subjectTopics: response.data?.subjectTopics,
+          submitted: true
         });
         console.log(response.data);        
       })
@@ -77,64 +90,82 @@ class CreateSubject extends React.Component {
       });
   }
 
+  // Create new subject page
+  newSubject = () => {
+    this.setState({
+      subjectName: "",
+      tutorialNumbers: "",
+      groupAssessment: "",
+      semester: "",
+      subjectTopics: "",
+      username: "",
+      submitted: false
+    });
+    this.componentDidMount();
+  }
+
   render() {
     return (
       <div style={{textAlign: "center", maxWidth: '100%', fontFamily: "Times New Roman"}} className="form">
-        <h3 style={{color: "light grey"}}>Create a New Subject</h3>
+        <h3>Create a New Subject</h3>
+        {this.state.submitted ? (
+          <div>
+            <p><i>You created a subject successfully!</i></p>
+            <Button size="small" variant="contained" onClick={this.newSubject}>{" "}Create a Subject{" "}</Button>
+          </div>
+        ) : (
           <div className="card">
-            <div className="form-group">
-              <label htmlFor="subject-name">Subject Name: </label>
-                <input className="form-control" style={{minWidth: '500px'}} type="text" name="subjectName" onChange={this.onChangeSubjectName}/>
-            </div>
-
-            <div className="form-group">
-              <label htmlFor="semester">Semester: </label>
-                <input className="form-control" style={{minWidth: '500px'}} type="text" name="semester" onChange={this.onChangeSemester}/>
-            </div>
-                  
-            <div className="form-group">
-              <label style={{marginLeft: "220px"}} htmlFor="tutorial numbers">Number of Tutorials:</label>
-              <select className="form-group border" style={{minWidth: "500px"}} onChange={this.onChangeTutorialNumbers}>
-                <option value="" disabled defaultValue={"Select your option"}></option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
-              </select>
-            </div>
-                  
-            <div className="form-group">
-              <label style={{marginLeft: "220px"}} htmlFor="group-assessment">Group Assessment:</label>
-              <select className="border" style={{minWidth: "500px"}} onChange={this.onChangeGroupAssessment}>
-                <option value="" disabled defaultValue={"Select your option"}></option>
-                <option value="Yes">Yes</option>
-                <option value="No">No</option>
-              </select>
-            </div>
-                  
-            {/* subject topics */}
-            <div className="form-group">
-              <label style={{marginLeft: "220px"}} htmlFor="subject-topics">Subject Topics:</label>
-              <textarea className="border" style={{minWidth: "500px"}} id="topics" name="topics" rows="5" placeholder="Please seperate each topic with a comma..." onChange={this.onChangeSubjectTopics}></textarea>
-            </div>
-
-            {/* assign tutors */}
-            <div className="form-group">
-              <label style={{marginLeft: "220px"}} htmlFor="subject-topics">Assign Tutors:</label>
-              <div id="checkboxes" style={{minWidth: "500px"}} >
-                {/* to be made dynamic with list of tutor names*/}
-                <label style={{display: "inline-flex"}}  for="one"><input style={{width: "auto"}} type="checkbox" id="one" value="John Doe"/>John Doe</label>
+              <div className="form-group">
+                <label htmlFor="subject-name">Subject Name: </label>
+                  <input className="form-control" style={{minWidth: '200px'}} type="text" name="subjectName" onChange={this.onChangeSubjectName} validations={[required]}/>
               </div>
-            </div>    
-      
-        </div>
-        <Button size="small" variant="contained" onClick={this.saveSubject}>Submit</Button>
+
+              <div className="form-group">
+                <label htmlFor="semester">Semester: </label>
+                  <input className="form-control" style={{minWidth: '200px'}} type="text" name="semester" onChange={this.onChangeSemester} validations={[required]}/>
+              </div>
+                    
+              <div className="form-group">
+                <label style={{marginLeft: "220px"}} htmlFor="tutorial numbers">Number of Tutorials:</label>
+                <select className="form-group border" style={{minWidth: "500px"}} onChange={this.onChangeTutorialNumbers} validations={[required]}>
+                  <option value="" disabled selected>Select your option</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                  <option value="6">6</option>
+                  <option value="7">7</option>
+                  <option value="8">8</option>
+                  <option value="9">9</option>
+                  <option value="10">10</option>
+                </select>
+              </div>
+                    
+              <div className="form-group">
+                <label style={{marginLeft: "220px"}} htmlFor="group-assessment">Group Assessment:</label>
+                <select className="border" style={{minWidth: "500px"}} onChange={this.onChangeGroupAssessment} validations={[required]}>
+                  <option value="" disabled selected>Select your option</option>
+                  <option value="Yes">Yes</option>
+                  <option value="No">No</option>
+                </select>
+              </div>
+              
+              <div className="form-group">
+                <label style={{marginLeft: "220px"}} htmlFor="subject-topics">Subject Topics:</label>
+                <textarea className="border" style={{minWidth: "500px"}} id="topics" name="topics" rows="5" placeholder="Please seperate each topic with a comma..." onChange={this.onChangeSubjectTopics} validations={[required]}></textarea>
+              </div>
+
+              <div className="form-group">
+                <label style={{marginLeft: "220px"}} htmlFor="subject-topics">Assign Tutors:</label>
+                <div id="checkboxes" style={{minWidth: "500px"}} >
+                  {/* to be made dynamic with list of tutor names*/}
+                  <label style={{display: "inline-flex"}}  for="one"><input style={{width: "auto"}} type="checkbox" id="one" value="John Doe"/>John Doe</label>
+                </div>
+              </div>
+          <Button size="small" variant="contained" onClick={this.saveSubject}>Submit</Button>
+          </div>
+        )}
       </div>
     );
   };
