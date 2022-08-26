90cd048a5dfab31371b9deb4b558963c88bd405d
nicholasfinos <nicholasfinos@gmail.com>
2022-08-26 20:28:28 +1000

    added form submit method

--- src/components/studentProfile.js
+++ src/components/studentProfile.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./studentProfile.css";
 import Form from "react-validation/build/form";
-import image from "../media/blank.webp";
+// import image from "../media/blank.webp";
 
 class StudentProfile extends React.Component {
   constructor(props) {
@@ -20,14 +20,14 @@ class StudentProfile extends React.Component {
   componentDidMount() {
     const URL = String(this.props.match.path);
     const name = String(URL.substring(URL.lastIndexOf("/") + 1, URL.length));
-    this.setState({studentName: name});
+    this.setState({ studentName: name });
   }
 
   // onChangeGroupAssessment(e) {
   //   this.setState({groupAssessment: e.target.value});
   // }
 
-  newSubject = ()  => {
+  newSubject = () => {
     this.setState = ({
       studentName: "",
     });
@@ -52,51 +52,66 @@ class StudentProfile extends React.Component {
     })
   }
 
+  updateStudentProfile = () => { 
+    // not sure how to actually link front and back end
+    fetch('http://localhost:8080/api/student/', {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        username: "student",
+        year: this.state.year,
+        course: this.state.course
+      })
+    })
+  }
+
   render() {
 
     const { studentName } = this.state;
 
     return (
-      <Form style={{textAlign: "center", maxWidth: '100%', fontFamily: "Times New Roman"}} className="form">
+      <div style={{ textAlign: "center", maxWidth: '100%', fontFamily: "Times New Roman" }} className="form">
         <div className="big-container">
           <div className="container">
             <div className="title"> My Profile <i>{studentName}</i></div>
             <div className="content">
-              <form action="#">
-              <div class="user-details">
-          <div class="input-box">
-            <span class="details">Full Name</span>
-            <input type="text" placeholder="Enter your name" required/>
-          </div>
-          <div class="input-box">
-            <span class="details">Preferred Name</span>
-            <input type="text" placeholder="Enter your email" required/>
-          </div>
-          <div class="input-box">
-          <label className="details">Year:</label>
-            <select className="info-card" value={this.state.year} onChange={this.handleYearChange}>
-              <option value="yr1">Yr 1</option>
-              <option value="yr2">Yr 2</option>
-              <option value="yr3">Yr 3</option>
-            </select>
-          </div>  
-          <div class="input-box">
-          <label className="details">Degree:</label>
-            <select className="info-card" value={this.state.degree} onChange={this.handleDegreeChange}>
-              <option value="deg1">Software Engo</option>
-              <option value="deg2">Finance</option>
-              <option value="deg3">Philosophy</option>
-            </select>
-          </div>
-        </div>
-        <div class="button">
-          <input type="submit" value="Register"/>
-        </div>
+              <form onSubmit={this.updateStudentProfile}>
+                <div class="user-details">
+                  <div class="input-box">
+                    <span class="details">Full Name</span>
+                    <input type="text" placeholder="Enter your name" required />
+                  </div>
+                  <div class="input-box">
+                    <span class="details">Preferred Name</span>
+                    <input type="text" placeholder="Enter your email" required />
+                  </div>
+                  <div class="input-box">
+                    <label className="details">Year:</label>
+                    <select className="info-card" value={this.state.year} onChange={this.handleYearChange}>
+                      <option value="yr1">Yr 1</option>
+                      <option value="yr2">Yr 2</option>
+                      <option value="yr3">Yr 3</option>
+                    </select>
+                  </div>
+                  <div class="input-box">
+                    <label className="details">Degree:</label>
+                    <select className="info-card" value={this.state.degree} onChange={this.handleDegreeChange}>
+                      <option value="deg1">Software Engo</option>
+                      <option value="deg2">Finance</option>
+                      <option value="deg3">Philosophy</option>
+                    </select>
+                  </div>
+                </div>
+                <div class="button">
+                  <input type="submit" value="Register" />
+                </div>
               </form>
             </div>
           </div>
         </div>
-      </Form>
+      </div>
     );
   };
 }
